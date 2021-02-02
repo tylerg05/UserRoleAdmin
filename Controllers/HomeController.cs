83f3df94ea04a33c3cc2084c93c999ffff1442a4
jgrissom <jgrissom91@gmail.com>
2021-02-02 16:03:04 -0600

    Home Controller - Create a UserManager object to query the data collection - Update the Index Method to send enumerated Users to the view - Home/Index view enumerates user collection

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,27 +6,22 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using UserRoleAdmin.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace UserRoleAdmin.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private UserManager<AppUser> userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> usrMgr)
         {
             _logger = logger;
+            userManager = usrMgr;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public ViewResult Index() => View(userManager.Users);
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
