ba668448e2b4d98cd7716ad4e2b5d745c8f8a8f7
jgrissom <jgrissom91@gmail.com>
2021-02-07 22:09:27 -0600

    Create Roles - RoleAdmin/Create controller method  handles http get - RoleAdmin/Create view - RoleAdmin/Create controller method handles http post

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 
 namespace UserRoleAdmin.Controllers
 {
@@ -12,5 +14,32 @@ namespace UserRoleAdmin.Controllers
         }
 
         public IActionResult Index() => View(roleManager.Roles);
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Required]string name)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = await roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            return View(name);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
     }
 }
